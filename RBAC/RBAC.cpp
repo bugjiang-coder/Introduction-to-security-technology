58827473d5e0f55cfcad4d58a7f97618877e01ff
yjr_win_mechrevo <1355604358@qq.com>
2021-11-30 11:35:41 +0800

    new try about cpp

--- RBAC/RBAC.cpp
+++ RBAC/RBAC.cpp
@@ -1,31 +1,131 @@
 #include <iostream>
+#include <map>
 using namespace std;
 
-class user
+class USER
 {
 public:
-    int id;
-    char name[];
+    USER(int id, const char *name);
+    int u_ID;
+    const char *u_name;
+    void show();
 };
 
-class role
+USER::USER(int id, const char *n)
+{
+    u_ID = id;
+    u_name = n;
+}
+
+void USER::show()
+{
+    cout << "ID: " << u_ID << "\tname: " << u_name << endl;
+}
+
+
+class ROLE
 {
 public:
-    int roleID;
-    char roleName[];
+    ROLE(int id, const char *name);
+    int r_ID;
+    const char *r_name;
+    void show();
 };
 
-class permission
+ROLE::ROLE(int id, const char *n)
+{
+    r_ID = id;
+    r_name = n;
+}
+
+void ROLE::show()
+{
+    cout << "role ID: " << r_ID << "\trole name: " << r_name << endl;
+}
+
+
+class PERMISSION
 {
 public:
-    int permissionID;
-    char permissionName[];
+    PERMISSION(int id, const char *name);
+    int p_ID;
+    const char *p_name;
+    void show();
 };
 
-// åˆ©ç”¨c++çš„æ•°æ®STLåº“å®šä¹‰ä¸Šé¢ä¸‰ä¸ªå…ƒç´  çš„å…³ç³»  
+PERMISSION::PERMISSION(int id, const char *n)
+{
+    p_ID = id;
+    p_name = n;
+}
 
+void PERMISSION::show()
+{
+    cout << "permission ID: " << p_ID << "\tpermission name: " << p_name << endl;
+}
+
+
+/*
+// ÀûÓÃc++µÄÊı¾İSTL¿â¶¨ÒåÉÏÃæÈı¸öÔªËØ µÄ¹ØÏµ
+// Ê¹ÓÃÒ»¸ö
+class URtable
+{
+public:
+    map<user, role> entry;
+    void add(user u, role r);
+    void print();
+};
+
+void URtable::add(user u, role r)
+{
+    entry.insert(make_pair(u, r));
+}
+
+void URtable::print()
+{
+    for (auto it = entry.begin(); it != entry.end(); it++);
+        // cout << it->first.ID << "\t<--->\t" << it->second.roleID << endl;
+}
+*/
 int main()
 {
-    cout << "Hello, world!" << endl;
+    // map<string, int> m;
+    // m["abc"] = 5;
+    // m["cdf"] = 6;
+    // m["b"] = 1;
+    // for (auto it = m.begin(); it != m.end(); it++)
+    //     cout << it->first << "<->" << it->second << endl;
+    const char *a = "user1";
+    const char b[6] = "user1";
+    const char c[6] = "user1";
+    const char d[6] = "user1";
+    USER *U1 = new USER(1, a);
+    USER u2(2, "user2");
+    U1->show();
+    u2.show();
+
+    ROLE r1(1,"role1");
+    r1.show();
+
+    PERMISSION p1(1,"permission1");
+    p1.show();
+
+    map<USER*, ROLE*> entry;
+    // Ææ¹îµÄÇé¿ö£¬mapÖĞ²»ÄÜ²åÈë ×Ô¼º¶¨ÒåµÄclass µ«ÊÇ¿ÉÒÔ²åÈëclassµÄÖ¸Õë
+    // ¶øÆä ¾ÍÊÇÃ»ÓĞÖ´ĞĞµÄ´íÎó´úÂë Ö»Òª´æÔÚÔÚÎÄ¼şÖĞ£¨classÖĞ£©  g++Ò²»á¸øÎÒ±¨´í
+    entry.insert(make_pair(U1, &r1));
+
+    for (auto it = entry.begin(); it != entry.end(); it++)
+        cout << it->first->u_ID << "\t<--->\t" << it->second->r_ID << endl;
+
+
+    // USER u2(2,&b);
+    // USER u3(3,&c);
+    // USER u4(4,&d);
+
+    // u2.show();
+    // u3.show();
+    // u4.show();
+
     return 0;
 }
